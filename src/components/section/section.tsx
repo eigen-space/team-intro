d98f902d0f4e95b68a22a30f1b766d748031a70e
Nikita Agupov <nktrsk@gmail.com>
2020-06-07 16:33:08 +0300

    #4 common: added section into product

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as styles from './section.module.scss';
+import styles from './section.module.scss';
 
 interface Props {
     title?: string;
--- src/modules/main/components/sections/product/product.tsx
+++ src/modules/main/components/sections/product/product.tsx
@@ -4,20 +4,24 @@ import { Slider } from '../../../../../components/slider/slider';
 import products from '../../../../../assets/data/main/products.json';
 import { Slide } from './slide/slide';
 import { ProductSummary } from './types/product-summary';
+import { Section } from '../../../../../components/section/section';
 
 interface Props {
 }
 
 export class Product extends React.PureComponent<Props> {
+    private title = 'Cases';
 
     render(): React.ReactNode {
 
         return (
-            <div className={styles.product}>
-                <Slider>
-                    {(products as ProductSummary[]).map(product => <Slide key={product.info.name} {...product}/>)}
-                </Slider>
-            </div>
+            <Section title={this.title}>
+                <div className={styles.product}>
+                    <Slider>
+                        {(products as ProductSummary[]).map(product => <Slide key={product.info.name} {...product}/>)}
+                    </Slider>
+                </div>
+            </Section>
         );
     }
 }
