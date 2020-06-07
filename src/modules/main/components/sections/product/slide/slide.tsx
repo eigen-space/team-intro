86d8a1ebe36865410f14e7c74e10bf826156f6bb
Nikita Agupov <nktrsk@gmail.com>
2020-06-07 17:31:20 +0300

    #4 common: added slide markup

--- src/modules/main/components/sections/product/slide/slide.tsx
+++ src/modules/main/components/sections/product/slide/slide.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Company } from '../types/company';
 import { Info } from '../types/product-summary';
 import { Review } from '../types/review';
+import styles from './slide.module.scss';
 
 interface Props {
     company: Company;
@@ -12,10 +13,28 @@ interface Props {
 export class Slide extends React.PureComponent<Props> {
 
     render(): React.ReactNode {
-        const { company } = this.props;
+        const { company, info, review } = this.props;
 
         return (
-            <div>{company.name}</div>
+            <div className={styles.slide}>
+                <div>
+                    <div>
+                        <p>{company.name}</p>
+                        <p>{company.description}</p>
+                    </div>
+                    <div>
+                        <p>{review.description}</p>
+                        <p>— {review.author}, {review.role}</p>
+                    </div>
+                </div>
+                <div>
+                    <p>{info.name}</p>
+                    <p>{info.task.description}</p>
+                    <ul>
+                        {info.task.features.map(f => (<li>{f}</li>))}
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
