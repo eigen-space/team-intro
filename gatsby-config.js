3490619f04a6e82bc74e3fe3ffa5607d8fcc5a0a
Kalashnikov Anton <tohasan@yandex.ru>
2020-07-18 17:17:31 +0300

    Revert "#2 common: added experiments with markdown as data store"

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,6 @@ module.exports = {
     plugins: [
         'gatsby-transformer-sharp',
         'gatsby-plugin-sharp',
-        'gatsby-plugin-sass',
-        {
-            resolve: 'gatsby-source-filesystem',
-            options: { name: 'main', path: `${__dirname}/src/assets/content` }
-        },
-        'gatsby-transformer-remark'
+        'gatsby-plugin-sass'
     ]
 };
